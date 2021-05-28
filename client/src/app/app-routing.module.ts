36e28b8c0c84b797d7f2eda1b2bd4e8df5b96326
unknown <ganesh.wanare24@gmail.com>
2021-05-28 15:18:16 +0530

    End of the section 07

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,3 +1,6 @@
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { TestErrorComponent } from './errors/test-error/test-error.component';
 import { AuthGuard } from './guards/auth.guard';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
@@ -21,7 +24,10 @@ const routes: Routes = [
 
     ]
   },
-  { path: '**', component: HomeComponent, pathMatch: 'full' }
+  { path: 'errors', component: TestErrorComponent},
+  { path: 'not-found', component: NotFoundComponent},
+  { path: 'server-error', component: ServerErrorComponent },
+  { path: '**', component: NotFoundComponent, pathMatch: 'full' }
 ];
 
 @NgModule({
